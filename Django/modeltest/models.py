54a5d4d9a9a19ea51ef97215cb21a52a10f0711d
Akash Nandi <akashnandi0@gmail.com>
2021-04-29 14:37:42 +0530

    djangomod

---
+++ Django/modeltest/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    number_of_comments = models.IntegerField()
+    number_of_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.headline
+
+
+class Dog(models.Model):
+    name = models.CharField(max_length=200,default="")
+    data = models.CharField(max_length=500,default="")
+
+    def __str__(self):
+        return self.name
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):
+        return "%s the place" % self.name
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s the restaurant" % self.place.name
+
+class Waiter(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s the waiter at %s" % (self.name, self.restaurant)
+
+
+class Person(models.Model):
+    name = models.TextField(max_length=100)
+    email = models.EmailField()
+    mobile = models.TextField(max_length=100)
+class Aadhar(models.Model):
+    person = models.OneToOneField(Person,on_delete=models.CASCADE)
+    signature = models.TextField()
+    adhar_no = models.TextField(max_length=100)
